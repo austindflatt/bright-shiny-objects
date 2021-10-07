2f668ef39273c41959a1af2cad9a9defa14ac691
Austin Flatt <austindflatt@gmail.com>
2021-10-07 10:55:20 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// ┌─────────────────┐
+// │ Your code here! │
+// └─────────────────┘
+
+
+
+
+
+
+
+
+
+
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │ Code used for testing. Do not modify!                                   │                         
+// │                                                                         │
+const isDef = (arg) => arg === 'function';
+const pass = () => undefined;
+// │                                                                         │
+module.exports.createUser = isDef(typeof createUser) ? createUser : pass;
+module.exports.setAge = isDef(typeof setAge) ? setAge : pass;
+module.exports.incrementAge = isDef(typeof incrementAge) ? incrementAge : pass;
+module.exports.fixCar = isDef(typeof fixCar) ? fixCar : pass;
+module.exports.addGrades = isDef(typeof addGrades) ? addGrades : pass;
+module.exports.getDataType = isDef(typeof getDataType) ? getDataType : pass;
+module.exports.addTodo = isDef(typeof addTodo) ? addTodo : pass;
+module.exports.addSong = isDef(typeof addSong) ? addSong : pass;
+module.exports.updateReportCard = isDef(typeof updateReportCard) ? updateReportCard : pass;
+// │                                                                         │
+// └─────────────────────────────────────────────────────────────────────────┘
